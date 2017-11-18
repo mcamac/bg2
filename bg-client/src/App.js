43b4c3737dc2ecf1ce958144dc0886ac313d079a
Martin Camacho <martin@kensho.com>
2017-11-17 20:14:29 -0500

    Add client scaffolding

---
+++ bg-client/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">HMR worsdasdks!</p>
+      </div>
+    )
+  }
+}
+
+export default App
