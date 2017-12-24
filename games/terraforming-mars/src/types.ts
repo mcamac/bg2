ad29696ee1f7d57f63835ba0eddf312b962d1b0f
Anthony Liu <anthony.liu@kensho.com>
2017-12-23 19:21:03 -0500

    wip

--- games/terraforming-mars/src/types.ts
+++ games/terraforming-mars/src/types.ts
@@ -27,7 +27,6 @@ export interface Card {
   actionText?: string
   effectText?: string
   placeTiles?: boolean
-  // requires?: ((state: GameState) => boolean)[]
   requires?: any[][]
   discounts?: any
   resourceHeld?: CardResource
