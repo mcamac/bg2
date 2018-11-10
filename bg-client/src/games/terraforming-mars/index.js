d37180853fb69b3816331a4badddbeee2f7540c3
Martin Camacho <martin@mcamac.com>
2018-11-10 18:14:36 -0500

    Give standard projects a hover (#47)

--- bg-client/src/games/terraforming-mars/index.js
+++ bg-client/src/games/terraforming-mars/index.js
@@ -682,18 +682,26 @@ const Awards = () => (
   </Box>
 )
 
+const StandardProjectWrapper = styled(Flex)`
+  cursor: pointer;
+
+  &:hover {
+    background: #f5f5f5;
+  }
+`
+
 let StandardProjects = props => (
   <Box style={{fontSize: 14}}>
     <Box mb="3px" style={{borderBottom: '1px solid #555'}}>
       Standard Projects
     </Box>
     {toPairs(STANDARD_PROJECTS).map(([key, project]) => (
-      <Flex key={key} onClick={() => props.onClickProject(key)}>
+      <StandardProjectWrapper key={key} onClick={() => props.onClickProject(key)}>
         <Box w={80} flex="1 1 auto">
           {project.name}
         </Box>
         <Box>{project.cost > 0 && project.cost}</Box>
-      </Flex>
+      </StandardProjectWrapper>
     ))}
   </Box>
 )
