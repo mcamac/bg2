7d633ad48f173a4beb712dad1212fd996bd3f9fd
Martin Camacho <martin@mcamac.com>
2019-03-12 16:26:50 -0500

    Bump webpack and typescript

--- bg-client/webpack.config.js
+++ bg-client/webpack.config.js
@@ -1,13 +1,12 @@
 const webpack = require('webpack')
 const path = require('path')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   devtool: 'source-map',
   entry: {
-    app: ['@babel/polyfill', 'react-hot-loader/patch', './src/index'],
+    app: ['@babel/polyfill', './src/index'],
   },
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -24,11 +23,15 @@ module.exports = {
       },
       {
         test: /\.tsx?$/,
-        loader: 'ts-loader',
-        options: {
-          // disable type checker - we will use it in fork plugin
-          transpileOnly: true,
-        },
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              // disable type checker - we will use it in fork plugin
+              transpileOnly: true,
+            },
+          },
+        ],
       },
     ],
     noParse: [/node_modules\/ws/],
@@ -44,6 +47,5 @@ if (process.env.NODE_ENV === 'production') {
     app: ['@babel/polyfill', './src/index'],
   }
 
-  module.exports.plugins.push(new UglifyJSPlugin())
   module.exports.plugins.push(new HtmlWebpackPlugin({template: 'index.html', inject: false}))
 }
